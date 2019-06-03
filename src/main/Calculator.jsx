d9022a9c02da95104eb94914933056f0a04ebbdd
Augusto Scher <augustoscher@gmail.com>
2019-06-03 01:04:49 -0300

    35 - bitton component

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import './Calculator.css'
 
+import Button from '../components/Button'
+
 export default class Calculator extends React.Component {
   render() {
     return (
       <div className="calculator">
-
-      </div>
+        <Button label="AC"/>
+        <Button label="/"/>
+        <Button label="7"/>
+        <Button label="8"/>
+        <Button label="9"/>
+        <Button label="*"/>
+        <Button label="4"/>
+        <Button label="5"/>
+        <Button label="6"/>
+        <Button label="-"/>
+        <Button label="1"/>
+        <Button label="2"/>
+        <Button label="3"/>
+        <Button label="+"/>
+        <Button label="0"/>
+        <Button label="."/>
+        <Button label="="/>
+      </div>  
     )
   }
 }
